49169940d9f98ad61a27f53d6cc3aad9d4b64761
Sasha <skalitsenia@mail.ru>
2024-03-14 12:16:23 +0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 import * as path from 'path'
 
 export default defineConfig({
-  base: 'Achieving-success',
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
-  },
+    base: '/Achieving-success',
+    plugins: [react()],
+    resolve: {
+        alias: [{find: '@', replacement: path.resolve(__dirname, 'src')}],
+    },
 })
